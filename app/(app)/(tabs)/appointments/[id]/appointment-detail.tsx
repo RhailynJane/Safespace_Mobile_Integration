602dd6a2467647e3df5d4fc354e2e5e2fd3c6d02
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-09-06 20:01:54 -0700

    add appt details

---
+++ app/(app)/(tabs)/appointments/[id]/appointment-detail.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AppointmentDetail = () => {
+  return (
+    <View>
+      <Text>Appointment Detail  </Text>
+    </View>
+  )
+}
+
+export default AppointmentDetail
+
+const styles = StyleSheet.create({})
