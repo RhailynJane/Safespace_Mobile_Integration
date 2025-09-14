6fd10c36b93e211c2e9f5799625ebd7c50a44b3b
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-09-14 11:39:43 -0700

    remove backend on profile settings

--- app/(app)/(tabs)/profile/index.tsx
+++ app/(app)/(tabs)/profile/index.tsx
@@ -14,7 +14,7 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import CurvedBackground from "../../../components/CurvedBackground";
+import CurvedBackground from "../../../../components/CurvedBackground";
 
 /**
  * ProfileScreen Component
